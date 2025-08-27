0a22e88aead80449f5d20fec5ed1c15a4da5bd41
josepmc99 <josepmc94@gmail.com>
2025-08-27 21:50:43 +0200

    prueba 12

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -2,15 +2,21 @@
 import ProjectList from "@/data/projects";
 import PropertyDetail from "@/components/PropertyDetail";
 
+interface ProjectPageProps {
+  params: {
+    id: string;
+  };
+}
+
+// Genera las rutas estáticas
 export async function generateStaticParams() {
   return ProjectList.map((p) => ({
-    id: p.id.toString(),
+    id: p.id.toString(), // siempre string
   }));
 }
 
 // 🔑 Haz la función async y espera los params si es necesario
-export default function ProjectPage(props: any) {
-  const { params } = props;
+export default function ProjectPage({ params }: ProjectPageProps) {
   const project = ProjectList.find((p) => p.id.toString() === params.id);
 
   if (!project) {
