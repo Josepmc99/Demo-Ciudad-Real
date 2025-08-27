ad7b625b0b8745005da5267237222c57ddf30cbf
josepmc99 <josepmc94@gmail.com>
2025-08-27 20:06:01 +0200

    Noveno commit

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,13 +1,19 @@
 import ProjectList from "@/data/projects";
 import PropertyDetail from "@/components/PropertyDetail";
 
+type ProjectPageProps = {
+  params: {
+    id: string; // siempre string en rutas dinámicas
+  };
+};
+
 export async function generateStaticParams() {
   return ProjectList.map((p) => ({
-    id: p.id.toString(), // siempre string
+    id: p.id.toString(), // genera rutas estáticas como strings
   }));
 }
 
-export default function ProjectPage({ params }: { params: { id: string } }) {
+export default function ProjectPage({ params }: ProjectPageProps) {
   const project = ProjectList.find((p) => p.id.toString() === params.id);
 
   if (!project) {
